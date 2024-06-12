726031c7ea69ba5159c1e087735f1239009c6d32
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2024-06-11 17:16:36 -0700

    changed character icon and banner font color

--- my-website/src/components/Characters.js
+++ my-website/src/components/Characters.js
@@ -1,4 +1,4 @@
-import { CodeIcon } from "@heroicons/react/solid";
+import { UserIcon } from "@heroicons/react/solid";
 import React from "react";
 import { projects } from "../data";
 
@@ -8,9 +8,9 @@ export default function Characters() {
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <UserIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            My Projects
+            Characters
           </h1>
         </div>
         <div className="flex flex-wrap -m-4">
--- my-website/src/components/Navbar.js
+++ my-website/src/components/Navbar.js
@@ -11,19 +11,19 @@ export default function Navbar() {
           </a>
         </a>
 
-        <a className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 font-medium text-white">
+        <a className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 font-medium text-black">
           <h1 style={{ fontSize: "40px" }}>Love & Other Monsters</h1>
         </a>
         
         <a
           href="https://twitter.com/heartXstudios"
-          className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:bg-white rounded text-base mt-4 md:mt-0">
           <img src="./twitter.png" alt="twt" style={{ width: "50px", height: "50px" }}/>
         </a>
 
         <a
           href="https://www.instagram.com/heartxstudios?igsh=MzRlODBiNWFlZA=="
-          className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:bg-white rounded text-base mt-4 md:mt-0">
           <img src="./instagram.png" alt="ig" style={{ width: "50px", height: "50px" }}/>
         </a>
       </div>
