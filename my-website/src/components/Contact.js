c437b135a152344774cafc4d05f22b06f4d0289a
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2024-06-20 16:45:43 -0700

    added about us section in contact.js

--- my-website/src/components/Contact.js
+++ my-website/src/components/Contact.js
@@ -3,20 +3,29 @@ import React from "react";
 export default function Contact() {
   return (
     <section id="contact" className="relative">
-        <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-              
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-1/2 md:w-1/2 flex flex-col md:ml-10 w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-2 font-medium title-font">
+            About Us
+          </h2>
+          <p style={{ fontSize: "20px" }}>
+            Heart Crossed Studios is an independent game studio, founded in 2023 by UCSC Alumnis during our undergrad studies! Our
+            primary goal is developing heartfelt stories and experiences for underrepresented communities, with a focus on the POC and
+            LGBTQ+ community. We are currently a team of 7, with comprehensive experience in creative, technical, and marketing fields.
+          </p>
+
+        </div>
         <form
           netlify
           name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+          className="lg:w-1/2 md:w-1/2 flex flex-col md:ml-10 w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-2 font-medium title-font">
+            Contact Us
           </h2>
-          <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+          <p style={{ fontSize: "20px" }}>
+            Thank you for your time and interest in our game. If you have any feedback for us, we’d love to hear it!
           </p>
-          <div className="relative mb-4">
+          <div className="relative mb-4 mt-2">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
             </label>
@@ -56,7 +65,6 @@ export default function Contact() {
             Submit
           </button>
         </form>
-        
       </div>
     </section>
   );
--- my-website/src/components/Skills.js
+++ my-website/src/components/Skills.js
@@ -9,7 +9,7 @@ export default function Skills() {
         <div className="text-center mb-20">
           <ChipIcon className="w-10 inline-block mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Skills &amp; Technologies
+            Game Snippets
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
@@ -17,7 +17,7 @@ export default function Skills() {
             possimus est.
           </p>
         </div>
-        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+        {/* <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
             <div key={skill} className="p-2 sm:w-1/2 w-full">
               <div className="bg-gray-800 rounded flex p-4 h-full items-center">
@@ -28,7 +28,7 @@ export default function Skills() {
               </div>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </section>
   );
