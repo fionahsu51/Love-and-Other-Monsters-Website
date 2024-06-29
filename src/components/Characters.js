c9a571c0eddfa408b862215c2c1fd029b69f48fe
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2024-06-29 15:40:19 -0700

    updated character text and sprite layout

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -12,25 +12,21 @@ export default function Characters() {
             Characters
           </h1>
       </center>
-      <div className="container px-5 mx-auto flex sm:flex-nowrap flex-wrap mr-10">
-        <div className="flex flex-col">
         {projects.map((project) => (
-            <div>
-              <div className="flex p-4 h-full items-center" style={{ fontSize: "25px" }}>
-                <div style={{ width: "600px" }}>
+            <div className="container px-5 mx-auto flex sm:flex-nowrap flex-wrap mr-10">
+              <div className="lg:w-1/2 md:w-1/2 flex flex-col md:ml-10 w-full md:py-8 mt-8 md:mt-0" style={{ fontSize: "25px" }}>
                   <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">{project.name}</h1>
                   <p>{project.info}</p>
                   <br></br>
                   <p>Pronouns: {project.pronouns}</p>
                   <p>Likes: {project.likes}</p>
                   <p>Dislikes: {project.dislikes}</p>
-                </div>
+              </div>
+                <div className="lg:w-1/2 md:w-1/2 flex flex-col md:ml-10 w-full md:py-8 mt-8 md:mt-0">
                   <img src={project.sprite} alt="character sprite" style={{ width: "500px", height: "500px" }} />
                 </div>  
             </div>
-          ))}
-        </div>
-      </div>
+          ))} 
     </section>
   );
 }
