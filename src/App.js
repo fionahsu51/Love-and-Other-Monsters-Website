35caf19b82d007029a62227b20ce7ed409b90d1a
Malachi Maldonado <m.maldonado1792@gmail.com>
2024-06-30 17:27:22 -0700

    testing background img

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="text-gray-400 bg-gray-900 body-font">
+    <div className="text-gray-400 body-font">
       <Navbar />
       <Intro />
       <Characters />
