1297ada1b6751aa722101f11bee1ed8b62c6c732
Malachi Maldonado <m.maldonado1792@gmail.com>
2024-08-05 22:22:26 -0700

    navbar no longer covers logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-navbarcurtain bg-contain top-0 z-10 w-screen" style={{position:"fixed"}}>
+    <div className="navbar bg-navbarcurtain bg-contain top-0 z-10 w-screen" style={{'max-height':"60px",position:"fixed"}}>
       <div className="container navcols mx-auto flex flex-wrap md:flex-row items-center">
         <div className="mb-4 md:mb-0">
           <a href="#Intro" 
-            className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:scale-110 rounded text-base mt-4 md:mt-0">
+            className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:scale-110 rounded text-base md:mt-0 sm:mt-1">
             <img src="./hcstudio_logo.png " alt="hclogo" style={{ 'max-width': "50px", 'max-height': "50px" }} />
           </a>
         </div>
@@ -18,13 +18,13 @@ export default function Navbar() {
         <div className="navsocialicon">
           <a
           href="https://twitter.com/heartXstudios"
-          className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:scale-110 rounded text-base mt-0 md:mt-0">
+          className="inline-flex items-center border-0 lg:py-1 md:py-0 sm:py-0 px-3 focus:outline-none hover:scale-110 rounded text-base mt-0 sm:mt-0 md:mt-0">
           <img src="./twitter.png" alt="twt" style={{ 'max-width': "40px", 'max-height': "40px" }}/>
         </a>
 
         <a
           href="https://www.instagram.com/heartxstudios?igsh=MzRlODBiNWFlZA=="
-          className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:scale-110 rounded text-base mt-0 md:mt-0">
+          className="inline-flex items-center border-0 lg:py-1 md:py-0 sm:py-0 px-3 focus:outline-none hover:scale-110 rounded text-base mt-0 md:mt-0 sm:mt-0">
           <img src="./instagram.png" alt="ig" style={{ 'max-width': "40px", 'max-height': "40px" }}/>
         </a>
         </div>
